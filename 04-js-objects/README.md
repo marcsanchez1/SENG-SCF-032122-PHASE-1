108441edf4adbbb16ecaf76b5d0446fc48b9a308
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-03-29 13:53:45 -0400

    initial materials for todays objects lecture

---
+++ 04-js-objects/README.md
@@ -0,0 +1,32 @@
+# JavaScript Objects
+
+### Lecture Outline
+
+- Review: arrays and callback functions
+- Discuss creating and manipulating objects
+- Practice traversing an object 
+
+### Objectives
+
+- Create and manipulate objects in JavaScript
+
+- Access and modify objects using dot and bracket notation (oka property accessors)
+
+- Use the delete operator to remove a property from an object
+
+- Use enumerators to access and manipulate data in objects
+
+### Warm Up Question
+
+&emsp; 💡 What are the limitations of arrays that objects help us to address?
+
+### Resources
+
+&emsp; [CodeAcademy - Learn Javascript: Objects Cheatsheet](https://www.codecademy.com/learn/game-dev-learn-javascript-objects/modules/game-dev-learn-javascript-objects/cheatsheet)
+
+&emsp; [MDN Property Accessors](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors)
+
+&emsp; [The Modern JavaScript Tutorial - Prototypal Inheritance](https://javascript.info/prototype-inheritance)
+
+&emsp; [The Essential Guide to JavaScript Iterators](https://www.javascripttutorial.net/es6/javascript-iterator/)
+
---
+++ 04-js-objects/reviewAssignment.js
@@ -0,0 +1,26 @@
+//  === Callbacks and arrow functions ===
+
+// reminder that our traditional function as converted to arrow function looks like this:
+// function funcName(){} => const funcName = () => {}
+
+
+// Declare the following functions using arrow function syntax:
+
+// 1. Write a function 'add' with 2 parameters, num1 and num2. Return the sum of the two parameters. 
+
+// 2. Write a function 'sub' with 2 parameters, num1 and num2. Return the absolute (positive) value of the two parameters subtracted from one another.
+
+// 3. Write a function 'compute' that will receive a callback function. Invoke the callback function within 'compute' with two arguments provided: 6 and 3.
+
+// Invoke compute 2 times, each time passing it each of the functions defined for steps 1-2 (add and sub): 
+
+//  3a. - Example: compute(add) ➞ 9 
+//  3b. - Example: compute(sub) ➞ 3
+
+// 4. Rewrite #3a (or #3b) to directly pass in an arrow function as the callback argument returning the expected values i.e., do not use a variable as the callback function.
+
+// 5. Invoke setInterval(), using the function keyword this time, to print a message to the console. The second argument should be set to 3000 milliseconds.
+
+
+
+
